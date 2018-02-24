1e01d12d1baa507fe6b491062c4af2a1a5b0e620
Islam Abdalla <islam.a.elamin@gmail.com>
2018-02-24 20:57:21 +0200

    Getting the winner

--- src/main/kotlin/com/narbase/game/AppPresenter.kt
+++ src/main/kotlin/com/narbase/game/AppPresenter.kt
@@ -12,7 +12,6 @@ class AppPresenter : Presenter() {
 
     override fun onViewCreated(view: View) {
         statusTextView?.text = "Turn: $turn player"
-
     }
 
     fun onCellClicked(index: Int) {
@@ -20,11 +19,39 @@ class AppPresenter : Presenter() {
         if (cell?.text?.isNotEmpty() == true || gameEnded) return
         cell?.text = turn
         flipTurn()
+        getWinner()?.let {
+            statusTextView?.text = "$it is the winner!"
+            gameEnded = true
+        }
+
     }
 
     private fun flipTurn() {
         turn = if (turn == "X") "O" else "X"
         statusTextView?.text = "Turn: $turn player"
     }
+
+    private val winningCombinations = arrayListOf(
+            arrayListOf(0, 1, 2),
+            arrayListOf(3, 4, 5),
+            arrayListOf(6, 7, 8),
+            arrayListOf(0, 3, 6),
+            arrayListOf(1, 4, 7),
+            arrayListOf(2, 5, 8),
+            arrayListOf(0, 4, 8),
+            arrayListOf(2, 4, 6)
+    )
+
+    private fun getWinner(): String? {
+        winningCombinations.forEach {
+            if (cells[it[0]]?.text?.isNotEmpty() == true &&
+                    cells[it[0]]?.text == cells[it[1]]?.text &&
+                    cells[it[0]]?.text == cells[it[2]]?.text )
+                return cells[it[0]]?.text
+        }
+        return null
+    }
+
+
 
 }
