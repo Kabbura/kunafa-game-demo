c48549f43da5467bc09765124dee4ed36d4ca7e6
Islam Abdalla <islam.a.elamin@gmail.com>
2018-02-24 10:58:22 +0200

    Basic cells UI

---
+++ src/main/kotlin/com/narbase/game/App.kt
@@ -0,0 +1,58 @@
+package com.narbase.game
+
+import com.narbase.kunafa.core.components.*
+import com.narbase.kunafa.core.components.layout.JustifyContent
+import com.narbase.kunafa.core.components.layout.LinearLayout
+import com.narbase.kunafa.core.dimensions.dependent.matchParent
+import com.narbase.kunafa.core.dimensions.independent.px
+import com.narbase.kunafa.core.drawable.Color
+
+fun main(args: Array<String>) {
+    AppView().setup()
+}
+
+class AppView {
+    fun setup() {
+        page {
+            horizontalLayout {
+                width = matchParent
+                margin = 16.px
+                justifyContent = JustifyContent.Center
+                verticalLayout {
+                    horizontalLayout {
+                        addCell(0)
+                        addCell(1)
+                        addCell(2)
+
+                    }
+                    horizontalLayout {
+                        addCell(3)
+                        addCell(4)
+                        addCell(5)
+
+                    }
+                    horizontalLayout {
+                        addCell(6)
+                        addCell(7)
+                        addCell(8)
+
+                    }
+                }
+            }
+        }
+    }
+
+    private fun LinearLayout.addCell(index: Int) {
+        verticalLayout {
+            width = 30.px
+            height = 30.px
+            background = if (index.rem(2) == 0) Color.rgb(220, 220, 220) else Color.white
+            justifyContent = JustifyContent.Center
+            textView {
+                width = matchParent
+                textSize = 18.px
+                textAlign = TextView.TextAlign.Center
+            }
+        }
+    }
+}
